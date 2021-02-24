bf14adb6846c095c055fcb0fb20788c687ef7376
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-02-24 09:11:15 +0530

    Test Case: Single CharNonEmpty String_Sol

--- StringCalculatorSolution.java
+++ StringCalculatorSolution.java
@@ -3,7 +3,12 @@ package TDD;
 public class StringCalculatorSolution {
 
 	public int Add(String input) {
-		return 0;
+		if (input.isEmpty())
+		{
+			return 0;
+		}
+		return Integer.parseInt(input);
+		
 	}
 
 }
