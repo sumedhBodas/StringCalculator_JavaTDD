f0869651612dd23cfe8dc55f5e5521cc47cdf9c5
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-02-24 09:20:54 +0530

    Test Case: CommaSepartedString_Sol

--- StringCalculatorSolution.java
+++ StringCalculatorSolution.java
@@ -3,9 +3,15 @@ package TDD;
 public class StringCalculatorSolution {
 
 	public int Add(String input) {
-		if (input.isEmpty())
+		if (input.length() == 0)
 		{
 			return 0;
+		}
+		
+		if (input.contains(","))
+		{
+			return Integer.parseInt(input.split(",")[0].trim()) 
+					 + Integer.parseInt(input.split(",")[1].trim());
 		}
 		return Integer.parseInt(input);
 		
